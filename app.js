39c164e45e4b6d9bed0240d4726d1b98e5a860c3
mharris162 <mharris162@student.alamo.edu>
2024-09-17 17:39:32 -0500

    concatenated string variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/// if" conditional statement (1)
 const age = 21;
 
 if (age >= 21) {
@@ -6,8 +7,7 @@ if (age >= 21) {
     console.log("Come back when you're 21!");
 }
 
-//  end of "if" conditional statement 
-
+//  "if" conditional statement (2)
 
 const dice1 = 6;
 const dice2 = 4;
@@ -16,3 +16,14 @@ if (dice1 === 6 || dice2 ===6) {
     console.log ("You got a 6!")
 }
 /// || means or, && means you need 2 conditions to both be true
+
+///string concatenations
+
+const name = "MacKenzie";
+const myAge = 26; 
+const both = name + myAge; 
+
+console.log(`Hi! My name is ${name} and I am ${myAge} years old`);
+console.log(both);
+document.getElementById("demo").innerHTML =
+`Hi! My name is ${name} and I am ${myAge} years old`;

d73dcde7cc630ab8e1ea3d6b03e7558bc174519b
mharris162 <mharris162@student.alamo.edu>
2024-09-17 17:17:27 -0500

    conditional statements

---
+++ app.js
@@ -0,0 +1,18 @@
+const age = 21;
+
+if (age >= 21) {
+    console.log("You are good!");
+} else {
+    console.log("Come back when you're 21!");
+}
+
+//  end of "if" conditional statement 
+
+
+const dice1 = 6;
+const dice2 = 4;
+
+if (dice1 === 6 || dice2 ===6) {
+    console.log ("You got a 6!")
+}
+/// || means or, && means you need 2 conditions to both be true

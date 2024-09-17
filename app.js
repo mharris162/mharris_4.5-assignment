8d2da955e645df3aa0f398c3c7222e2f1be53610
mharris162 <mharris162@student.alamo.edu>
2024-09-17 18:14:42 -0500

    number method

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ if (dice1 === 6 || dice2 ===6) {
 }
 /// || means or, && means you need 2 conditions to both be true
 
-///string concatenations
+///string cncatenations
 
 const name = "MacKenzie";
 const myAge = 26; 
@@ -39,3 +39,21 @@ let animals = "Cats, Dogs, Fish";
 let some = animals.slice (0,3);
 document.getElementById("demo2").innerHTML =
 `I have a pet ${some}`;
+
+
+
+function spliter(num1, num2) {
+    console.log(num1 /= num2);
+}
+
+spliter(37.83, 4);
+
+// Number Method
+
+let bill = 9.4575
+console.log(bill.toFixed(2))
+
+
+
+
+
